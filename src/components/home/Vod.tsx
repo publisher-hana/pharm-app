4dcc6df4e0a3fdbe20044e4d7c296665d0a75e6d
lhn <hanaya1208@gmail.com>
2025-06-25 17:03:51 +0900

    upload

--- src/components/home/Vod.tsx
+++ src/components/home/Vod.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { BtnAll, MainSection } from './home.style'
 import { SecTitle } from '@/styles/common.style'
+import Slider from 'react-slick'
 
 const Vod = () => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   return (
     <>
       <MainSection>
         <SecTitle>임상강좌 VOD</SecTitle>
         <BtnAll>전체보기</BtnAll>
+        <Slider {...settings}>
+          <div></div>
+        </Slider>
       </MainSection>
     </>
 
